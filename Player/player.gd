deec8def80b141d05e97580fafab4078510ea2fb
Lucca T <luccatbordim@gmail.com>
2023-11-04 20:24:36 -0300

    Inimigos para teste
    Adição de inimigos para testar

--- Player/player.gd
+++ Player/player.gd
@@ -6,6 +6,7 @@ const JUMP_VELOCITY = 4.5
 
 #Basic Variables
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") # Get the gravity from the project settings to be synced with RigidBody nodes.
+@export var mouse_sensitivity = 0.01
 
 #Gun Variables
 
@@ -14,37 +15,12 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") # Get th
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-"""
-var max_speed = 8
-var mouse_sensitivity = 0.002
-
-func get_input():
-	var input_dir = Vector3()
-	if Input.is_action_pressed("move_forwards"):
-			input_dir += -global_transform.basis.z
-	
-	if Input.is_action_pressed("move_backwards"):
-		input_dir += global_transform.basis.z
-	
-	if Input.is_action_pressed("move_left"):
-		input_dir += -global_transform.basis.x
-		
-		if Input.is_action_pressed("move_right"):
-			input_dir += global_transform.basis.x
-	input_dir = input_dir.normalized()
-	return input_dir
-
-
-func _physics_process(delta):
-	velocity += gravity * delta
-	var desired_velocity = get_input() * max_speed
-	velocity.x = desired_velocity.x
-	velocity.x = desired_velocity.z
-	velocity = move_and_slide(velocity, Vector3.UP, true)
-"""
 
 func _unhandled_input(event):
-	pass
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		$Pivot.rotate_x(-event.relative.y * mouse_sensitivity)
+		$Pivot.rotation.x = clamp($Pivot.rotation.x, -1.5, 1.5)
 
 func _physics_process(delta):
 	# Add the gravity.
