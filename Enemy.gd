0df5ee7f97ffd6f31f8a2beae58dcd495f97240d
Lucca T <luccatbordim@gmail.com>
2023-11-05 22:34:44 -0300

    Adição de inimigos
    Falta apenas fazer eles andarem atrás do player

--- Enemy.gd
+++ Enemy.gd
@@ -6,6 +6,15 @@ const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+@export var health: float = 20
 
+var _path: Array[Vector3] = []
 
 func _physics_process(delta: float) -> void:
+  #if _path.length():
+    #var direction = (_path.pop_front() - global_position).normalized()
+    #move_and_slide(direction * SPEED)
+  pass
+
+func set_path(target) -> void:
+  pass
