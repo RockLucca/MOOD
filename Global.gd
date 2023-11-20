fcbcf503461eb190a43fb22fd64574554c99e9b4
Lucca T <luccatbordim@gmail.com>
2023-11-20 15:47:08 -0300

    Adição de elementos da HUD

--- Global.gd
+++ Global.gd
@@ -1,4 +1,20 @@
 extends Node
 
-var ammo = 100
+#Level variables
+var score = 0
+var current_level = 1
+
+#Player Stats
+var player_armor: int = 100 
+var player_health: int = 100
+
+#Guns Ammo
 var current_weapon = "pistol"
+var chainsaw_ammo = 100       #1
+var pistol_ammo = 10          #2
+var shotgun_ammo = 14         #3
+var rifle_ammo = 0            #4
+var rocket_launcher_ammo = 2  #5
+
+var ammo = 2
+var lives = 3
--- HUD/hud.gd
+++ HUD/hud.gd
@@ -6,6 +6,7 @@ var fire_rate = 1.0
 func _ready():
 	$Weapons.animation_finished.connect(_on_AnimatedSprite2D_animation_finished)
 	$Weapons.play(Global.current_weapon + "_idle")
+	$Weapons.play(Global.current_weapon + "_icon")
 
 func _process(delta):
 	time_since_last_shot += delta
@@ -14,6 +15,7 @@ func _process(delta):
 	if Global.current_weapon != "chainsaw" and Global.ammo <= 0:
 		Global.current_weapon = "chainsaw"
 		$Weapons.play("chainsaw_idle")
+		$Weapons.play("chainsaw_icon")
 	
 	if Input.is_action_just_pressed("shoot") and can_shoot:
 		
@@ -27,7 +29,7 @@ func _process(delta):
 		if Global.current_weapon != "chainsaw":
 			if Global.ammo > 0:
 				Global.ammo -= 1
-				
+	
 	match Global.current_weapon:
 		"pistol":
 			fire_rate = 3.0
@@ -39,6 +41,24 @@ func _process(delta):
 			fire_rate = 2.0
 		_:
 			fire_rate = 1.0
-			
+	
+	update_health_label()
+	update_armor_label()
+	update_ammo_label()
+
+
+func update_health_label():
+	$Healf_value.text = str(Global.player_health)
+
+func update_armor_label():
+	$Armor_value.text = str(Global.player_armor)
+
+func update_score_label():
+	$SCORE.text = str(Global.player_score)
+
+func update_ammo_label():
+	$Ammo_value.text = str(Global.ammo)
+
 func _on_AnimatedSprite2D_animation_finished():
 	$Weapons.play(Global.current_weapon + "_idle")
+	$Weapons.play(Global.current_weapon + "_icon")
