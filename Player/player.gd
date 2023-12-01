b1ded8c5cd7bd07767223597a4c7478561d05f78
getuliobr <getulioregis@alunos.utfpr.edu.br>
2023-12-01 15:31:02 -0300

    começo fix joystick

--- Player/player.gd
+++ Player/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 #Variables
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") # Get the gravity from the project settings to be synced with RigidBody nodes.
-@export var mouse_sensitivity = 0.01
+@export var mouse_sensitivity: float = 1
 @export var SPEED = 5.0
 @export var JUMP_VELOCITY = 4.5
 
@@ -12,16 +12,26 @@ const _blood_partile_res = preload("res://Particles/blood_particle.tscn")
 
 #Functions
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	add_to_group("Player")
 
+func rotate_camera(dir_vector: Vector2):
+	dir_vector = dir_vector.normalized()
+	rotate_y(-dir_vector.x * mouse_sensitivity)
+	$Pivot.rotate_x(-dir_vector.y * mouse_sensitivity)
+	$Pivot.rotation.x = clamp($Pivot.rotation.x, -1.5, 1.5)
+
 func _unhandled_input(event):
-	if event is InputEventMouseMotion:
-		rotate_y(-event.relative.x * mouse_sensitivity)
-		$Pivot.rotate_x(-event.relative.y * mouse_sensitivity)
-		$Pivot.rotation.x = clamp($Pivot.rotation.x, -1.5, 1.5)
-	if event is InputEventMouseButton and event.pressed and event.button_index == 1:
-		print(event.position)
+	if event is InputEventAction:
+		event.get_action_strength(event.action)
+		
+	if OS.has_feature("mobile"):
+		return
+	#if event is InputEventMouseMotion:
+		#print(event.relative.x, event.relative.y)
+		#rotate_camera(event.relative)
+	#if event is InputEventMouseButton and event.pressed and event.button_index == 1:
+		#print(event.position)
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -31,6 +41,14 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var aim_dir = Input.get_vector("ui_left2", "ui_right2", "ui_up2", "ui_down2")
+	rotate_camera(aim_dir)
+	
+	
+	if Input.is_action_just_pressed("ui_left2"):
+		print(123)
+	#print(InputEventAction.new().get_action_strength("ui_left2"))
+	
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -40,6 +58,9 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	
+	
+	
 
 func shoot(gun):
 	var camera3d = $Pivot/Camera3D
