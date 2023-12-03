704b9d820e87d5b553629deeba2ef0778c00eac4
getuliobr <getulioregis@alunos.utfpr.edu.br>
2023-12-03 01:39:41 -0300

    adicionar funcionalidade nos menus e botao voltar

--- Menu/menu.gd
+++ Menu/menu.gd
@@ -1,20 +1,29 @@
 extends Control
 
+@onready var curr_scene = $Main
+
+func change_screen(scene):
+	curr_scene.visible = false
+	curr_scene = scene
+	curr_scene.visible = true
+
 func _on_jogar_pressed():
 	get_tree().change_scene_to_file("res://Map Scenes/world.tscn")
 
-
 func _on_instrucoes_pressed():
-	pass # Replace with function body.
+	change_screen($Instrucoes)
 
 
 func _on_opcoes_pressed():
-	pass # Replace with function body.
+	change_screen($Opcoes)
 
 
 func _on_creditos_pressed():
-	pass # Replace with function body.
+	change_screen($Creditos)
 
 
 func _on_sair_pressed():
 	get_tree().quit()
+
+func _on_voltar_pressed() -> void:
+	change_screen($Main)
