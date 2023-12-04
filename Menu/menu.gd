7cef8430160aa79eadaf9c88a1b25fadb97590e2
getuliobr <getulioregis@alunos.utfpr.edu.br>
2023-12-04 17:26:14 -0300

    creditos

--- Menu/menu.gd
+++ Menu/menu.gd
@@ -6,6 +6,11 @@ func change_screen(scene):
 	curr_scene.visible = false
 	curr_scene = scene
 	curr_scene.visible = true
+	
+func _process(delta: float) -> void:
+	if curr_scene == $Creditos:
+		await get_tree().create_timer(1).timeout
+		$Creditos/CreditosText.global_position.y -= 1
 
 func _on_jogar_pressed():
 	await get_tree().create_timer(0.6).timeout
@@ -15,16 +20,13 @@ func _on_instrucoes_pressed():
 	await get_tree().create_timer(0.6).timeout
 	change_screen($Instrucoes)
 
-
 func _on_opcoes_pressed():
 	await get_tree().create_timer(0.6).timeout
 	change_screen($Opcoes)
 
-
 func _on_creditos_pressed():
 	await get_tree().create_timer(0.6).timeout
 	change_screen($Creditos)
-
 
 func _on_sair_pressed():
 	await get_tree().create_timer(0.6).timeout
