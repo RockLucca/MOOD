0f8a350b8854c647d845bb3b7f65092fb88a355f
Lucca T <luccatbordim@gmail.com>
2023-12-19 14:12:54 -0300

    Update do level e correçao de bugs
    - Correção de bug do som da motosserra
    - Correção de texturas e sombras
    - Correção de bug de boost infinito se teleportar com ele ativo

--- Map Scenes/teleport.gd
+++ Map Scenes/teleport.gd
@@ -4,4 +4,7 @@ extends Area3D
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
+		Global.boost_damage = 1
+		Global.boost_firerate = 1
+		Global.boost_velocity = 1
 		get_tree().change_scene_to_file(_scene)
