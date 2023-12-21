ae4bf8f7bdeec5c6bf466a53819aad05c939616e
getuliobr <getulioregis@alunos.utfpr.edu.br>
2023-12-20 21:34:48 -0300

    soltar o mouse na tela de vitoria

--- Menu/win_screen.gd
+++ Menu/win_screen.gd
@@ -1,5 +1,6 @@
 extends Control
 
 func _on_timer_timeout() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	Global.play_credits = true
 	get_tree().change_scene_to_file("res://Menu/menu.tscn")
